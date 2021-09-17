70a46002cb9b0d6dfa3febaa05e8b0aa87331b76
shivchander <shiv.yadav@ebixcash.com>
2021-09-17 16:00:43 +0530

    Commit 2

--- app/src/main/java/com/shiv/exo/MainActivity.kt
+++ app/src/main/java/com/shiv/exo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.shiv.exo
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 import com.google.android.exoplayer2.MediaItem
@@ -28,37 +29,47 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         exoplayerView = findViewById(R.id.mplayerView)
-        mediaDataSourceFactory =
-            DefaultDataSourceFactory(this, Util.getUserAgent(this, "mediaPlayerSample"))
-
-        val mediaSource = ProgressiveMediaSource.Factory(mediaDataSourceFactory).createMediaSource(
-            MediaItem.fromUri(STREAM_URL)
-        )
-
-        val mediaSourceFactory: MediaSourceFactory =
-            DefaultMediaSourceFactory(mediaDataSourceFactory)
-
-        simpleExoPlayer = SimpleExoPlayer.Builder(this)
-            .setMediaSourceFactory(mediaSourceFactory)
-            .setSeekBackIncrementMs(10000)
-            .setSeekForwardIncrementMs(10000)
-            .build()
-
-        simpleExoPlayer.addMediaSource(mediaSource)
-
-        // simpleExoPlayer.playWhenReady = true  //Play with btton
-        simpleExoPlayer.prepare()
-        simpleExoPlayer.play()  //Auto Play
-
-        exoplayerView.setShutterBackgroundColor(Color.TRANSPARENT)
-        exoplayerView.player = simpleExoPlayer
-        scaleGestureDetector =
-            ScaleGestureDetector(this, CustomOnScaleGestureListener(exoplayerView))
-        exoplayerView.requestFocus()
+        //initializeExo()
     }
+
+    private fun initializeExo() {
+
+
+        try {
+            mediaDataSourceFactory =
+                DefaultDataSourceFactory(this, Util.getUserAgent(this, "mediaPlayerSample"))
+
+            val mediaSource = ProgressiveMediaSource.Factory(mediaDataSourceFactory).createMediaSource(
+                MediaItem.fromUri(STREAM_URL)
+            )
+
+            val mediaSourceFactory: MediaSourceFactory =
+                DefaultMediaSourceFactory(mediaDataSourceFactory)
+
+            simpleExoPlayer = SimpleExoPlayer.Builder(this)
+                .setMediaSourceFactory(mediaSourceFactory)
+                .setSeekBackIncrementMs(10000)
+                .setSeekForwardIncrementMs(10000)
+                .build()
+
+            simpleExoPlayer.addMediaSource(mediaSource)
+
+            // simpleExoPlayer.playWhenReady = true  //Play with btton
+            simpleExoPlayer.prepare()
+            simpleExoPlayer.play()  //Auto Play
+
+            exoplayerView.setShutterBackgroundColor(Color.TRANSPARENT)
+            exoplayerView.player = simpleExoPlayer
+            scaleGestureDetector =
+                ScaleGestureDetector(this, CustomOnScaleGestureListener(exoplayerView))
+            exoplayerView.requestFocus()
+        } catch (e: Exception) {
+            Log.d("shiv","initialize error")
+        }
+    }
+
     companion object {
         const val STREAM_URL = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
-        //   const val STREAM_URL = "https://media.geeksforgeeks.org/wp-content/uploads/20201217163353/Screenrecorder-2020-12-17-16-32-03-350.mp4"
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
@@ -67,4 +78,46 @@ class MainActivity : AppCompatActivity() {
 
         return true
     }
+
+    private fun stopplayer() {
+        Log.d("zzzz", "called stopplayer")
+
+        try {
+            if (simpleExoPlayer != null) {
+                simpleExoPlayer.release()
+                simpleExoPlayer.clearVideoSurface()
+
+            }
+        } catch (e: Exception) {
+            Log.d("zzzz", "stop issue")
+        }
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("zzzz", "onPause")
+        stopplayer()
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("zzzz", "onStop")
+        stopplayer()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (Util.SDK_INT <= 23)   initializeExo()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT > 23) initializeExo()
+    }
+
+
+
 }
